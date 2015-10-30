a8f395c7484cab2168c369e041ced4f5c512da93
wkwyatt <wkwyatt1@gmail.com>
2015-10-30 15:46:01 -0400

    Added methods to picker to change color

--- PickerData/ViewController.swift
+++ PickerData/ViewController.swift
@@ -8,8 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let moodArray = ["Sleepy","Angry","Ecstatic","Cranky","Horny","Sarcastic","Moody","Content","Optimistic","Overjoyed","Giddy","Indifferent","Relaxed","Cynical"]
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //  how many pieces of data are in our component
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return moodArray.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return moodArray[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        var newBackgroundColor : UIColor
+        
+        switch row {
+        case 0,2,3,7:
+            newBackgroundColor = UIColor.yellowColor()
+        case 1,4:
+            newBackgroundColor = UIColor.redColor()
+        case 6:
+            newBackgroundColor = UIColor.grayColor()
+        case 8,10:
+            newBackgroundColor = UIColor.greenColor()
+        default:
+            newBackgroundColor = UIColor(red: 135/255, green: 214/255, blue: 1.0, alpha: 1.0)
+        }
+        
+        self.view.backgroundColor = newBackgroundColor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
